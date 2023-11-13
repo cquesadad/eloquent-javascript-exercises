91425acc2754475b606c6f83445e3f805c42a1ca
Carlos Quesada <cquesadad90@gmail.com>
2023-11-13 18:41:29 +0000

    first commit

---
+++ chapter-2/exercise-1/triangle_exercise.js
@@ -0,0 +1,11 @@
+//looping to create a triangle with # character 
+for ( let triangle = "#"; triangle.length <= 7; triangle += "#"){
+    console.log(triangle);
+}
+
+//looping to create a triangle with # character 
+// let triangle = "#"
+// for (let hashtagLength = 0; hashtagLength < 7; hashtagLength++){
+//     console.log(triangle);
+//     triangle = "#" + triangle;
+// }
